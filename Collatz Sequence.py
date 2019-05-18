8f41289f9b2f72466d957eca43b945a8ba5677ca
Gabriel <sagamubriel@gmail.com>
2019-05-17 23:40:21 -0400

    working...

--- Collatz Sequence.py
+++ Collatz Sequence.py
@@ -3,6 +3,7 @@
 
 def collatz(number):
     """An algorithm that no matter what positive number you give, it will return 1."""
+    number = int(number)
     if number % 2 == 0:
         print(number // 2)
         return number // 2
@@ -14,17 +15,14 @@ def collatz(number):
 
 
 def ask():
-    """..."""
-    num = int(input("Enter number: "))
-
-    try:
-        num
-
-    except ValueError or UnboundLocalError:
-        print("Just enter a number!")
-
+    """A function to ask the user for an integer."""
+    num = input("Enter a number: ")
     while num != 1:
-        num = collatz(num)
+        try:
+            num = collatz(num)
+        except ValueError:
+            print("Just enter a number!")
+
 
 
 ask()
