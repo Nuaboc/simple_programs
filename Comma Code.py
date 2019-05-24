56c6d4d07a5e76ff940d770b334f5a9c6c310a2c
Gabriel <31278920+Nuaboc@users.noreply.github.com>
2019-05-24 08:40:26 -0400

    gathering code in one file

--- Comma Code.py
+++ Comma Code.py
@@ -13,13 +13,41 @@ comma = ', '
 y = ', and '
 
 
-def code(lst):
+def join(lst):
     if len(lst) >= 2:
         out = comma.join(lst[:-1])
         out += y + lst[-1]
         print(out)
     else:
         print(lst[0])
+
+
+join(abc)
+
+join(lulu)
+
+join(pepe)
+
+join(sate)
+
+print('....................................................')
+
+# Solution 2
+
+
+def add(string):
+    return string
+
+
+def code(lst):
+    if len(lst) >= 2:
+        out = ' '
+        for i in lst[:-1]:
+            out += i + ', '
+        out += add('and ') + lst[-1]
+        print(out)
+    else:
+        print(lst)
 
 
 code(abc)
