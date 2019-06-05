b4875686c45b7678cc826639258db00eb8883643
Gabriel <sagamubriel@gmail.com>
2019-06-04 22:34:42 -0400

    learning tkinter...

--- tkinter_practice.py
+++ tkinter_practice.py
@@ -6,10 +6,17 @@ from tkinter import *
 
 # MAIN...
 
-window = TK()
+window = Tk()
 window.title("Computer Science Glossary")
+window.configure(background='teal')
+
+# image
+photo1 = PhotoImage(file='men_train.gif')
+Label(window, image=photo1, bg='black') .grid(row=0, column=0, sticky=E)
 
 # Label
+Label(window, text="Enter the word you would like  definition for:", bg='black', fg='white', font='none 12 bold')\
+    .grid(row=1, column=0, sticky=W)
 
 # Text entry box
 
