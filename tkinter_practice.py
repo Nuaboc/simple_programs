4f1869f2753d3302c56e7df57f71afdbcef76de7
Gabriel <sagamubriel@gmail.com>
2019-06-04 21:37:01 -0400

    testing tkinter

---
+++ tkinter_practice.py
@@ -0,0 +1,26 @@
+# Glossary Project
+
+from tkinter import *
+
+# Key down function
+
+# MAIN...
+
+window = TK()
+window.title("Computer Science Glossary")
+
+# Label
+
+# Text entry box
+
+# Summit button
+
+# Label
+
+# Text box
+
+# The Dictionary
+
+# MAIN LOOP...
+
+window.mainloop()
