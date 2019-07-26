b589514c4f6d1ada58d53ca45c8a9cbbc63d1116
Gabriel Martinez <31278920+Nuaboc@users.noreply.github.com>
2019-07-25 22:09:55 -0400

    .

---
+++ roman_num_converter.py
@@ -0,0 +1,17 @@
+# This program intend to convert romans numbers to decimal number and reverse.
+
+i = 1
+I = 'I'
+v = 5
+V = 'V'
+x = 10
+X = 'X'
+l = 50
+L = 'L'
+c = 100
+C = 'C'
+d = 500
+D = 'D'
+m = 1000
+M = 'M'
+
