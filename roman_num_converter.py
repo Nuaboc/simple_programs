a11618118d6baf4d6cf8a177704fc6bb73367fc9
Gabriel Martinez <31278920+Nuaboc@users.noreply.github.com>
2019-07-31 12:59:50 -0400

    starting good!

--- roman_num_converter.py
+++ roman_num_converter.py
@@ -1,17 +1,19 @@
+# python 3.7
+# by Gabriel Martinez
+
 # This program intend to convert romans numbers to decimal number and reverse.
 
-i = 1
-I = 'I'
-v = 5
-V = 'V'
-x = 10
-X = 'X'
-l = 50
-L = 'L'
-c = 100
-C = 'C'
-d = 500
-D = 'D'
-m = 1000
-M = 'M'
 
+roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+print("Give me a roman number to convert it to a cardinal number.")
+a = input()
+
+"""def convert():
+    if a
+
+convert(a)"""
+
+for key, value in roman_num.items():
+    if a in key:
+        print(value)
