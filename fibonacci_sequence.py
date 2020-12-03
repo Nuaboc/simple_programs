bcd523d1b46d39313f54d70d35aea5a79cfcfd01
Gabriel <sagamubriel@gmail.com>
2020-12-03 11:07:49 -0400

    Adding a script of the Fibonacci sequence

---
+++ fibonacci_sequence.py
@@ -0,0 +1,22 @@
+"""
+A code that develop the Fibonacci sequence.
+
+Each number in the sequence is the sum of the two numbers that precede it.
+"""
+
+num = int(input())
+
+
+def fibonacci(n):
+    """
+    Each number in the sequence is the sum of the two numbers that precede it.
+
+    Example: For 10 numbers, starting from 0;
+            returns 0, 1, 2, 5, 8, 13, 21, 34
+
+    :param n: is an input from the user, it should be a number
+    :return: a sequence based in the Fibonacci sequence.
+    """
+
+
+fibonacci(num)
