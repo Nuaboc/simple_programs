2d32fc951e3dd4653bb431618b12241092317f07
Gabriel <sagamubriel@gmail.com>
2021-01-20 23:24:39 -0400

    ...

---
+++ kivy/mobile_development/mobile_app.py
@@ -0,0 +1,32 @@
+"""
+Script to practice kivy for mobile development.
+"""
+
+import random
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+red = [1, 0, 0, 1]
+green = [0, 1, 0, 1]
+blue = [0, 0, 1, 1]
+purple = [1, 0, 1, 1]
+
+
+class MobileApp(App):
+    def build(self):
+        layout = BoxLayout(padding=5)
+        colors = [red, green, blue, purple]
+
+        for i in range(5):
+            btn = Button(text="Button #%s" % (i+1),
+                         background_color=random.choice(colors)
+                         )
+
+            layout.add_widget(btn)
+        return layout
+
+
+if __name__ == "__main__":
+    app = MobileApp()
+    app.run()
