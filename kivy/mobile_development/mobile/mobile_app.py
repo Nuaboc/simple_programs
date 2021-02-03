afdd26b608f6e0cbef715b735ea17d7e10124457
Gabriel Martinez <31278920+Nuaboc@users.noreply.github.com>
2021-02-03 11:21:19 -0400

    ...

--- kivy/mobile_development/mobile/mobile_app.py
+++ kivy/mobile_development/mobile/mobile_app.py
@@ -14,19 +14,20 @@ https://www.techwithtim.net/tutorials/kivy-tutorial/multiple-screens/
 Tutorial of a calculator with Kivy.
 From Real Python, found in:
 https://realpython.com/mobile-app-kivy-python/
+
+Despite the >>PEP 8: E402<< says "module level import not at top of file"
+for the kivy Config.set to affect the window, it should be imported and implemented
+before importing other modules
 """
 
-# Despite the >>PEP 8: E402<< says "module level import not at top of file"
-# for the kivy Config.set to affect the window, it should be imported and implemented
-# before importing other modules
-from kivy.config import Config
+# from kivy.config import Config
 # Limit the resize option to be False
-Config.set('graphics', 'resizable', 0)
+# Config.set('graphics', 'resizable', 0)
 # The width nad height will be set as for an iphone 11R
 # Fix the width of the window
-Config.set('graphics', 'width', 414)
+# Config.set('graphics', 'width', 414)
 # Fix the height of the window
-Config.set('graphics', 'height', 896)
+# Config.set('graphics', 'height', 896)
 
 # Now the rest of the modules may be imported
 # Standard library imports
@@ -35,6 +36,7 @@ import random
 # Third party imports
 from kivy.app import App
 from kivy.base import runTouchApp
+from kivy.uix.dropdown import DropDown
 from kivy.lang import Builder
 from kivy.properties import ListProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
@@ -54,6 +56,15 @@ class OpeningScreen(Screen):
 
 
 class MainScreen(Screen):
+    """dropdown = DropDown()
+    for index in range(5):
+        btn = Button(text='Value %d' % index, size_hint_y=None, height=44)
+
+        dropdown.add_widget(btn)"""
+    pass
+
+
+class CustomDropDown(DropDown):
     pass
 
 
@@ -109,6 +120,7 @@ class Root(ScreenManager):
 
 class MobileApp(App):
     """A class to setup for the app basics."""
+    test_var = 'coco'
 
     def build(self):
         """This method will be used as a root for all widgets."""
---
+++ kivy/mobile_development/mobile/snip_tests.py
@@ -0,0 +1,18 @@
+"""
+This file contains the functions and classes to help the NUI works as needed.
+
+Authors: Gabriel Martinez and Estalin Martinez
+Date: Jan 21, 2021
+"""
+
+word1 = "gas"
+
+word2 = 'trisas'
+
+word3 = 'bien'
+
+print("Me tire un %s." % word1)
+
+print("Me hice {}.".format(word2))
+
+print(f"Hiciste {word3}.")
