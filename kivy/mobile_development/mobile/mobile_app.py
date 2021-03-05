926192e98f1b2562317a05b0f4d2b66ef7ce3c71
Gabriel <sagamubriel@gmail.com>
2021-03-04 21:50:30 -0400

    ...

--- kivy/mobile_development/mobile/mobile_app.py
+++ kivy/mobile_development/mobile/mobile_app.py
@@ -17,7 +17,7 @@ before importing other modules
 """
 
 from kivy.config import Config
-Config.set('kivy', 'keyboard_mode', 'systemandmulti')
+#Config.set('kivy', 'keyboard_mode', 'systemandmulti')
 # Limit the resize option to be False
 # Config.set('graphics', 'resizable', 0)
 # The width nad height will be set as for an iphone 11R
@@ -28,31 +28,34 @@ Config.set('kivy', 'keyboard_mode', 'systemandmulti')
 
 # Now the rest of the modules may be imported
 # Standard library imports
+import time
 import random
 
 # Third party imports
 from kivy.app import App
-from kivy.base import runTouchApp
-from kivy.uix.dropdown import DropDown
-from kivy.lang import Builder
+# from kivy.base import runTouchApp
+# from kivy.uix.dropdown import DropDown
+# from kivy.lang import Builder
 from kivy.properties import ListProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.properties import StringProperty, ObjectProperty
+from kivy.properties import ObjectProperty
 # Make available the Button class from the kivy library
 from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.widget import Widget
+# from kivy.uix.label import Label
+# from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.boxlayout import BoxLayout
+# from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.popup import Popup
+from kivy.uix.vkeyboard import VKeyboard
 
-import time
-import random
+# import kivymd
 
-import kivymd
+# Local modules imports
+# import func
 
 
 class OpeningScreen(Screen):
@@ -66,9 +69,14 @@ class MainScreen(Screen):
 
         dropdown.add_widget(btn)"""
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        super(MainScreen, self).__init__()
         self.pc = PopUpContent()
         self.p = Popup(title='Popup test', content=self.pc, size_hint=(None, None), size=(400, 400))
+        self.prep_table()
+
+    def prep_table(self):
+        for i in range(1, 19):
+            self.ids.container.add_widget(TextInput(text='slot ' + str(i), multiline=False))
 
     def press(self):
         print(self.test_var.text)
@@ -88,14 +96,14 @@ class MainScreen(Screen):
 
         self.add_widget(self.keyboard)
 
-    def show_popup(self):
-
-        p.open()
-
 
 '''class CustomDropDown(DropDown):
     pass
 '''
+
+
+class Keyboard(VKeyboard):
+    kb = VKeyboard()
 
 
 class PopUpContent(FloatLayout):
@@ -111,7 +119,24 @@ class SettingsScreen(Screen):
 
 
 class AboutScreen(Screen):
-    pass
+
+    def __init__(self, **kwargs):
+        super(AboutScreen, self).__init__(**kwargs)
+        #self.scroll()
+
+    def scroll(self):
+        lets_scroll = ScrollView(size_hint=(1, None), do_scroll_x=False, do_scroll_y=True)
+
+        grid = GridLayout(col=3, size_hint_y=None)
+
+        grid.bind(minimum_height=grid.setter("height"))
+
+        for i in range(1, 50):
+            grid.add_widget(Button(text="slot " + str(i), size_hint_y=None, height=40))
+
+        lets_scroll.add_widget(grid)
+
+        self.add_widget(lets_scroll)
 
 
 # The ScreenManager controls moving between screens
