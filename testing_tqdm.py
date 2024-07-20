43f7adee2172990ff2bacc87d267b5896eefbd04
Gabriel Martínez <Sagamubriel@gmail.com>
2024-07-20 12:12:48 -0400

    minimal documentation added

--- testing_tqdm.py
+++ testing_tqdm.py
@@ -6,6 +6,16 @@ Date: July 20, 2024
 """
 
 from tqdm import tqdm
+"""
+Based on official documentation:
+
+Decorate an iterable object, returning an iterator which acts exactly
+like the original iterable, but prints a dynamically updating
+progressbar every time a value is requested.
+"""
 
 for i in tqdm(range(10000000)):
+    "range(million)"
     ...
+
+print("done!")
